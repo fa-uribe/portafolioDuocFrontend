0c21707bf8d651454b53cbf0fbe416d404f650d9
fa-uribe <thrasherxd16@gmail.com>
2023-04-09 17:47:20 -0400

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Constants from 'expo-constants';
+import { Text, View } from 'react-native';
+import SubjectList from './SubjectList.jsx'
+
+const Main = () => {
+    return (
+        <View>
+            <SubjectList />
+        </View>
+    )
+}
+
+export default Main;
---
+++ src/data/subjects.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        id: 1,
+        subjectName: "Math",
+    },
+    {
+        id: 2,
+        subjectName: "Programming"
+    },
+    {
+        id: 3,
+        subjectName: "Loco pepe"
+    }
+]
